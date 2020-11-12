8a835b5b0010328e0910d252b40d65f1ee6f0a3e
Beth Legesse <legessebl@gmail.com>
2020-11-11 18:41:21 -0600

    sw.js update

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,11 +3,11 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/index.js",
+  "/assets/js/index.js",
   "/manifest.webmanifest",
-  "/style.css",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
+  "/assets/css/style.css",
+  "/images/icon-192x192.png",
+  "/images/icon-512x512.png",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
   
