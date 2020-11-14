a240ea8ad7a18f449ccbf4dea96afbb529336c7a
Beth Legesse <legessebl@gmail.com>
2020-11-13 21:01:20 -0600

    service worker code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/assets/js/index.js",
-  "/manifest.webmanifest",
-  "/assets/css/style.css",
-  "/images/icon-192x192.png",
-  "/images/icon-512x512.png",
+  "/assets/css/styles.css",
+  "/assets/js/db.js",
+  "/assets/images/icon-192x192.png",
+  "/assets/images/icon-512x512.png",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
   
@@ -17,25 +17,26 @@ const FILES_TO_CACHE = [
 self.addEventListener("install", function (evt) {
   // pre cache image data
   evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
+    caches.open(CACHE_NAME).then((cache) => {return cache.addAll(FILES_TO_CACHE)
+    })
   );
+
+  self.skipWaiting();
     
   // pre cache all static assets
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
+  
 
   // tell the browser to activate this service worker immediately once it
   // has finished installing
-  self.skipWaiting();
+  
 });
 
 // activate
 self.addEventListener("activate", function(evt) {
   evt.waitUntil(
-    caches.keys().then(keyList => {
+    caches.keys().then((keyList) => {
       return Promise.all(
-        keyList.map(key => {
+        keyList.map((key) => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
@@ -49,34 +50,33 @@ self.addEventListener("activate", function(evt) {
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) {
-  if (evt.request.url.includes("/api/")) {
+self.addEventListener("fetch", (evt) => {
+  if (evt.request.url.includes("/api/") && evt.request.method === 'GET') {
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
+      caches.open(DATA_CACHE_NAME).then((cache) => {
         return fetch(evt.request)
-          .then(response => {
+          .then((response) => {
             // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
+              cache.put(evt.request, response.clone());
             }
 
             return response;
           })
-          .catch(err => {
+          .catch(()=> {
             // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
-      }).catch(err => console.log(err))
+      }).catch((err) => console.log(err))
     );
 
     return;
   }
 
   evt.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.match(evt.request).then(response => {
+    caches.match(evt.request).then(response => {
         return response || fetch(evt.request);
-      });
-    })
-  );
+      })
+    )
+  
 });
